0dc7b5af178a04021f0da527c2be93305fbd35bf
PewterZz <Peterns1609@gmail.com>
2023-12-18 19:08:00 +0700

    usage

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <bits/stdc++.h>
+#include "parser.tab.h" 
+
+extern "C" {
+    int yyparse();    
+    extern FILE *yyin; 
+}
+
 using namespace std;
 
 int main(int argc, char *argv[]){
+    if(argc < 2) {
+        cerr << "Usage: " << argv[0] << " <filename>\n";
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if(!file) {
+        cerr << "Cannot open file " << argv[1] << "\n";
+        return 1;
+    }
+    yyin = file;
+
+    yyparse();
+
+
+    fclose(file);
 
     return 0;
 }
